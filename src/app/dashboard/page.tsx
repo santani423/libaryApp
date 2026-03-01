e7ff19c6405d3b8cd4e7d58717cf4c71069a6e35
santani423 <santani423@gmail.com>
2026-03-02 06:00:00 +0700

    banner

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import Template from "@/components/layout/template";
 import { HeroBanner } from "@/components/section/HeroBanner";
+// import { useState } from "react";
 export default function DashboardPage() {
+//   const [bookCategory, setBookCategory] = useState<string | null>(null);
   return (
     <Template>
       <div className="custom-container flex justify-center items-center px-6 md:px-28 bg-white">
