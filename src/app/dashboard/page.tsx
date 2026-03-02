0f867bfb3ff0481cb36d0c90ea164d22fedb821a
santani423 <santani423@gmail.com>
2026-03-02 15:11:16 +0700

    icon

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import Template from "@/components/layout/template";
 import { HeroBanner } from "@/components/section/HeroBanner";
 import { BookCategory } from "@/components/section/BookCategory";
 import { RecommendationBook } from "@/components/section/RecommendationBook";
+import { PopularAuthors } from "@/components/section/PopularAuthors";
 // import { useState } from "react";
 export default function DashboardPage() {
 //   const [bookCategory, setBookCategory] = useState<string | null>(null);
@@ -11,6 +12,7 @@ export default function DashboardPage() {
         <HeroBanner />
         <BookCategory />
         <RecommendationBook />
+        <PopularAuthors />
       </div>
     </Template>
   );
