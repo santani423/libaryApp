79da6a4337928c937191b8aeebe0c3fecf016055
santani423 <santani423@gmail.com>
2026-03-04 01:34:17 +0700

    type books

--- src/components/section/BookLists.tsx
+++ src/components/section/BookLists.tsx
@@ -7,39 +7,44 @@ import type { Book } from "@/types/books";
 
 type BookListsProps = {
   title?: string;
+  books?: Book[];
 };
 
-export function BookLists({ title = "" }: BookListsProps, books: Book[]) {
-  const cards = Array.from({ length: 10 });
-  console.log("BookListsBookLists",books);
-  
+export function BookLists({ title = "", books = [] }: BookListsProps) {
   return (
     <section className="custom-container px-6 md:px-28 bg-white">
-
-      {title && (
-        <h2 className="text-lg font-semibold mb-4">{title}</h2>
-      )}
+      {title && <h2 className="text-lg font-semibold mb-4">{title}</h2>}
 
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4">
-        {cards.map((_, index) => (
-          <Card
-            key={index}
-            className="rounded-xl border border-gray-200 shadow-sm hover:shadow-md transition-all duration-300"
-          >
-            <CardContent className="p-3 flex flex-col gap-2">
-              <div className="w-full flex justify-center items-center bg-gray-100 h-20 rounded-md">
-                <Image
-                  src={Logo}
-                  alt={`Book ${index + 1}`}
-                  width={40}
-                  height={40}
-                />
-              </div>
+        {books.map((book, index) => {
+          const imageSrc = book.coverImage?.startsWith("data:")
+            ? book.coverImage
+            : `data:image/jpeg;base64,${book.coverImage}`;
 
-              <p className="text-sm font-medium text-left">Book {index + 1}</p>
-            </CardContent>
-          </Card>
-        ))}
+          return (
+            <Card
+              key={book.id ?? index}
+              className="rounded-xl border border-gray-200 shadow-sm hover:shadow-md transition-all duration-300"
+            >
+              <CardContent className="flex flex-col gap-2">
+                <div className="w-full flex justify-center items-center   h-32 rounded-md overflow-hidden">
+                  <Image
+                    src={imageSrc}
+                    alt={book.title ?? `Book ${index + 1}`}
+                    width={80}
+                    height={120}
+                    className="object-cover"
+                    unoptimized
+                  />
+                </div>
+
+                <p className="text-sm font-medium text-left">
+                  {book.title ?? `Book ${index + 1}`}
+                </p>
+              </CardContent>
+            </Card>
+          );
+        })}
       </div>
     </section>
   );
